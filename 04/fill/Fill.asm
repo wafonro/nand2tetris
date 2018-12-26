83b923a7b369b66ec71c6297a1b33d425c036805
wafonro <wagner.fonseca.rodrigues@gmail.com>
2018-12-26 18:44:42 +0100

    Chapter 04

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,50 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(BEGIN)
+@8096
+D = A
+@R0
+M = D
+@SCREEN
+D = A
+@R1
+M = D
+
+@KBD
+D = M
+@PRESSED
+D,JNE
+
+(RELEASED)
+@R0
+D = M
+@BEGIN
+D;JEQ
+@R0
+M = M-1
+@R1
+A = M
+M = 0
+@R1
+M = M+1
+@RELEASED
+0;JMP
+
+@BEGIN
+0;JMP
+
+(PRESSED)
+@R0
+D = M
+@BEGIN
+D;JEQ
+@R0
+M = M-1
+@R1
+A = M
+M = -1
+@R1
+M = M+1
+@PRESSED
+0;JMP
